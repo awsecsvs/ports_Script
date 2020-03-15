7f391da7912b1cca4f99fa632057cd6d1b0a1420
awsecsvs <wayne@awsecsvs.org>
2020-03-14 22:42:48 -0400

    Update ports_script.sh

--- ports_script.sh
+++ ports_script.sh
@@ -3,16 +3,15 @@
 # Usage enter the IP address of the target right after script name 
 # and the file name to write to
 
-
-if [ "$1" = "" ]; then
+if [ "$1" == "" ]
+then
     echo "Need to enter an IP address"
-    exit 2
-f1
-
-  
-if [ "$2" = "" ]; then
+    exit 1
+fi
+if [ "$2" == "" ]
+then
     echo "Provide full file path to the file to write results to"
-    exit 2
+    exit 1
 fi
 
 nmap -p- --min-rate=1000 -T4 $1 | grep ^[0-9] | cut -d '/' -f1 |tr '\n' ','|sed s/,$// > $2
